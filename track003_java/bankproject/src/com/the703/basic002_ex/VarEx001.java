880c1c80b512b443ab71de04dfec7efb882a57c5
jooyeap <eyoung071212@naver.com>
2026-03-27 18:18:59 +0900

    day002_end

--- track003_java/bankproject/src/com/the703/basic002_ex/VarEx001.java
+++ track003_java/bankproject/src/com/the703/basic002_ex/VarEx001.java
@@ -3,7 +3,13 @@ package com.the703.basic002_ex;
 public class VarEx001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int apple;
+		
+		apple = 1000;
+		System.out.println("사과가격은 " + apple + " 원입니다.\n");
+		
+		apple = 2000;
+		System.out.println("사과가격은 " + apple + " 원입니다.\n");
 
 	}
 
